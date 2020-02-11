1768e8071e564c04fa961431251952598823c21b
Brian Liang <Brian.Liang@solium.com>
2020-02-11 13:41:12 -0700

    First commit for PluraSight React Hooks course

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+const InputElement = () => {
+
+    const [inputText, setInputText] = useState("");
+
+    return <div>
+        <input 
+    onChange={(e) => {
+        setInputText(e.target.value)
+    }}
+    placeholder="Enter Some Text"/><br/>
+    {inputText}
+    </div>
+};
+
+export default InputElement;
